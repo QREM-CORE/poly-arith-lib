5b346487a734df4c4e4c1301780475360a1ea977
Kiet Le <ktlbusiness11@gmail.com>
2026-03-12 19:28:52 -0400

    PE_UNIT: Refine and Verify PE_UNIT (and PE0) (Supporting CWM, NTT, INTT so far) (TODO: ADD/SUB and DE/COMP) (#23)
    * PE_UNIT: Reconfig pe_unit to 8-ports
    
    * PE_UNIT: Add Twiddle Factor Synchronization Delay
    
    - Output PEs need to synchronize their twiddle factor inputs to align with their corresponding data operations
    
    * PE_UNIT: Fix proper Valid propogation
    
    - valid from input stage (STAGE 1) come from pe_unit valid input to input PEs
    - valid to output stage (STAGE 2) comes from the AND of the two input PEs
    - For CWM specifically, for PE3, output delay needs to be implemented to align with PE0
    
    * PE_UNIT: Fix add/sub mode with add/sub select
    
    * PE0: Fix comment
    
    * PE_UNIT: add/sub fix
    
    * PE_UNIT_VERIF: Init PE_UNIT Testbench & Verify CWM Functionality
    
    * PE_UNIT: Support Radix 2 in PE_UNIT
    
    * PE_UNIT: Verify R4 NTT Mode
    
    * PE_UNIT: Verify R2 NTT
    
    * PE_UNIT: Verify INTT R4 Functionality
    
    * PE_UNIT: Modify R2 INTT Routing & Verify R2 INTT

--- tb/pe0_tb.sv
+++ tb/pe0_tb.sv
@@ -106,10 +106,15 @@ module pe0_tb();
         coeff_t t;
 
         // 1. Calculate Expected Values
-        if (mode == PE_MODE_NTT || mode == PE_MODE_CWM) begin
+        if (mode == PE_MODE_NTT) begin
             t = mod_mul(b, w);
             exp.u = mod_add(a, t);
-            exp.v = mod_sub(a, t);
+            exp.v = mod_sub(a, t); // NTT: Top - Bottom
+        end
+        else if (mode == PE_MODE_CWM) begin
+            t = mod_mul(b, w);
+            exp.u = mod_add(a, t);
+            exp.v = mod_sub(t, a); // CWM: Bottom - Top (The PE0 Fix!)
         end
         else if (mode == PE_MODE_INTT) begin
             exp.u = mod_div2(mod_add(a, b));
